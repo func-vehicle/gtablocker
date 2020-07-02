40818d09081f4b180fc985c618ba892d4f71cad4
func_vehicle <56104255+func-vehicle@users.noreply.github.com>
2020-07-02 17:43:54 +1200

    Fixed all local IPs being blocked

--- setup.bat
+++ setup.bat
@@ -10,11 +10,11 @@ netsh advfirewall firewall delete rule name="GTA V Block"
 netsh advfirewall firewall delete rule name="GTA V Open"
 
 REM Create inbound rules
-netsh advfirewall firewall add rule name="GTA V Block" protocol=UDP dir=in action=block enable=no localport=6672,61455,61456,61457,61458 remoteip=0.0.0.0 profile=any program=%location%
-netsh advfirewall firewall add rule name="GTA V Block" protocol=TCP dir=in action=block enable=no localport=any remoteip=0.0.0.0 profile=any program=%location%
+netsh advfirewall firewall add rule name="GTA V Block" protocol=UDP dir=in action=block enable=no localport=6672,61455,61456,61457,61458 localip=0.0.0.0 remoteip=0.0.0.0 profile=any program=%location%
+netsh advfirewall firewall add rule name="GTA V Block" protocol=TCP dir=in action=block enable=no localport=any localip=0.0.0.0 remoteip=0.0.0.0 profile=any program=%location%
 netsh advfirewall firewall add rule name="GTA V Open" protocol=UDP dir=in action=allow enable=yes localport=any profile=any program=%location%
 netsh advfirewall firewall add rule name="GTA V Open" protocol=TCP dir=in action=allow enable=yes localport=any profile=any program=%location%
 
 REM Create outbound rules
-netsh advfirewall firewall add rule name="GTA V Block" protocol=UDP dir=out action=block enable=no localport=6672,61455,61456,61457,61458 remoteip=0.0.0.0 profile=any program=%location%
+netsh advfirewall firewall add rule name="GTA V Block" protocol=UDP dir=out action=block enable=no localport=6672,61455,61456,61457,61458 localip=0.0.0.0 remoteip=0.0.0.0 profile=any program=%location%
 PAUSE
